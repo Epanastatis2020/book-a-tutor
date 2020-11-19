ac82eaad352d428233795d3792e45c94315f2283
HomeAlone <luke.gallagher.cst@gmail.com>
2020-11-20 09:56:18 +1100

    14-sequelize-CRUD update

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,46 +1,46 @@
 $(document).ready(() => {
   // Getting references to our form and input
-  const signUpForm = $("form.signup");
-  const emailInput = $("input#email-input");
-  const passwordInput = $("input#password-input");
-  const userTypeInput = $("select#userType-input");
+  const signUpForm = $('form.signup');
+  const emailInput = $('input#email-input');
+  const passwordInput = $('input#password-input');
+  const userTypeInput = $('select#userType-input');
 
   // When the signup button is clicked, we validate the email and password are not blank
-  signUpForm.on("submit", event => {
+  signUpForm.on('submit', event => {
     event.preventDefault();
     console.log(`UserType: ${userTypeInput.val()}`);
     const userData = {
+      // TODO: add firstName, lastName
+      firstName: 'Me',
+      lastName: 'Myself',
       email: emailInput.val().trim(),
       password: passwordInput.val().trim(),
-      isTutor: userTypeInput.val() === 'student' ? false : true
+      userType: userTypeInput.val()
     };
 
+    // TODO: check firstName, lastName
     if (!userData.email || !userData.password) {
       return;
     }
     // If we have an email and password, run the signUpUser function
-    signUpUser(userData.email, userData.password, userData.isTutor);
-    emailInput.val("");
-    passwordInput.val("");
+    signUpUser(userData);
+    emailInput.val('');
+    passwordInput.val('');
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(email, password, isTutor) {
-    $.post("/api/signup", {
-      email: email,
-      password: password,
-      isTutor: isTutor
-    })
+  function signUpUser (user) {
+    $.post('/api/signup', user)
       .then(() => {
-        window.location.replace("/members");
+        window.location.replace('/members');
         // If there's an error, handle it by throwing up a bootstrap alert
       })
       .catch(handleLoginErr);
   }
 
-  function handleLoginErr(err) {
-    $("#alert .msg").text(err.responseJSON);
-    $("#alert").fadeIn(500);
+  function handleLoginErr (err) {
+    $('#alert .msg').text(err.responseJSON);
+    $('#alert').fadeIn(500);
   }
 });
