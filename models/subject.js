1e6e1c97df089499f6d696f159d8580f4759eaf4
ConA <65388616+Epanastatis2020@users.noreply.github.com>
2020-11-19 21:45:36 +1100

    fixed eslint issues

--- models/subject.js
+++ models/subject.js
@@ -1,26 +1,26 @@
 module.exports = function (sequelize, DataTypes) {
-  const Subject = sequelize.define('Subject', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    }
-  })
+    const Subject = sequelize.define('Subject', {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1],
+            },
+        },
+    });
 
-  Subject.associate = function (models) {
-    // Associating Subject with TutorSubjects
-    // When a Subject is deleted, also delete any associated TutorSubjects
-    Subject.hasMany(models.TutorSubject, {
-      onDelete: 'cascade'
-    })
-    // Associating Subject with Bookings
-    // When an Subject is deleted, also delete any associated Bookings
-    Subject.hasMany(models.Booking, {
-      onDelete: 'cascade'
-    })
-  }
+    Subject.associate = function (models) {
+        // Associating Subject with TutorSubjects
+        // When a Subject is deleted, also delete any associated TutorSubjects
+        Subject.hasMany(models.TutorSubject, {
+            onDelete: 'cascade',
+        });
+        // Associating Subject with Bookings
+        // When an Subject is deleted, also delete any associated Bookings
+        Subject.hasMany(models.Booking, {
+            onDelete: 'cascade',
+        });
+    };
 
-  return Subject
-}
+    return Subject;
+};
--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,12 +1,14 @@
-document.addEventListener("DOMContentLoaded", function () {
-  //this section is all for making the things draggable to the calendar
-  var Draggable = FullCalendarInteraction.Draggable;
-  var containerEl = document.getElementById("external-events");
+document.addEventListener('DOMContentLoaded', function () {
+    //this section is all for making the things draggable to the calendar
+    var Draggable = FullCalendarInteraction.Draggable;
+    var containerEl = document.getElementById('external-events');
 
-  new Draggable(containerEl, {
-    itemSelector: ".fc-event",
-    eventData: function (eventEl) {
-      return {
-        title: eventEl.innerText,
-      };
-    },
+    new Draggable(containerEl, {
+        itemSelector: '.fc-event',
+        eventData: function (eventEl) {
+            return {
+                title: eventEl.innerText,
+            };
+        },
+    });
+});
