f24049f92af5de269e4f3a907a5a4b0172833525
HomeAlone <luke.gallagher.cst@gmail.com>
2020-11-23 19:28:14 +1100

    removed commented out code

--- models/index.js
+++ models/index.js
@@ -19,7 +19,6 @@ fs.readdirSync(__dirname)
         return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
     })
     .forEach(function (file) {
-        // var model = sequelize['import'](path.join(__dirname, file));
         let model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
         db[model.name] = model;
     });
